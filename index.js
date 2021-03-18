c2f652e3e539c83bb62162762e9b5fde9aa03bf7
Jakson Cunha de Souza <jaksoncunha1@gmail.com>
2021-03-18 21:19:50 +0000

    updated

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 
 app.listen(3000, () => console.log('Server running on port 3000'))
 
-app.get('/atendimentos', (req, res) => res.send('Atendimentos'))
+app.get('/atendimentos', (req, res) => res.send('Atendimentos!'))
 
 
 
